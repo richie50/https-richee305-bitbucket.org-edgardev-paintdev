b5be19a5d1189f272f49cc5b0e9b84136a738b4e
Daniyal Javed <daniyaljavedk@gmail.com>
2015-11-26 05:32:53 +0000

    LineSegment.java created online with Bitbucket

---
+++ PaintDev/LineSegment.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.geom.Line2D;
+
+public class LineSegment {
+    private Line2D.Double coordinates;
+    private Color color;
+    private Stroke stroke;
+
+    public LineSegment(Line2D.Double coordinates, Color color, Stroke stroke) {
+        this.coordinates = coordinates;
+        this.color = color;
+        this.stroke = stroke;
+    }
+
+    public void draw(Graphics2D graphics2D) {
+        graphics2D.setColor(color);
+        graphics2D.setStroke(stroke);
+        graphics2D.draw(coordinates);
+    }
+}
